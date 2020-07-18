42bd25c9cbf1e1e1d6ac3fe3c20de2eff3dacaf2
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2020-07-18 13:05:36 -0300

    cambio en schools

--- internal/api/services/schools/schools.go
+++ internal/api/services/schools/schools.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Service interface {
-	GetSchools(searchText string, province string, department string) []domain.School
+	GetSchools(searchText string, province string, department string) []domain.SchoolApp
 }
 
 type service struct {
@@ -20,7 +20,15 @@ func NewService(database *db.Database) Service {
 	}
 }
 
-func (s *service) GetSchools(searchText string, province string, department string) []domain.School {
+func (s *service) GetSchools(searchText string, province string, department string) []domain.SchoolApp {
 	schools := crud.NewDatabaseSchoolRepo(s.database).GetSchools(searchText, province, department)
-	return schools
+	schoolsApp := make([]domain.SchoolApp, len(schools))
+	for i, school := range schools {
+		schoolsApp[i] = schoolToSchoolApp(school)
+	}
+	return schoolsApp
+}
+
+func schoolToSchoolApp(school domain.School) domain.SchoolApp {
+	return domain.SchoolApp{Name: school.Name, Province: school.Province, Department: school.Department, Location: school.Location}
 }
